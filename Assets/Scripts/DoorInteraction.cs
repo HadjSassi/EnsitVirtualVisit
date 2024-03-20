ced1e9507ddeee6baded38bf53bacdfd965b69e0
mahdi <mahdihadjsassi@fsb.u-carthage.tn>
2024-03-20 18:32:44 +0000

    Door Interaction is fixed

---
+++ Assets/Scripts/DoorInteraction.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DoorInteraction : MonoBehaviour
+{
+    private static readonly int ApplyInteraction = Animator.StringToHash("ApplyInteraction");
+    private static readonly string PlayerTag = "Interaction";
+    private Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == PlayerTag)
+        {
+            animator.SetBool(ApplyInteraction,true);
+        }
+    }
+     private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == PlayerTag)
+        {
+            animator.SetBool(ApplyInteraction,false);
+        }
+    }
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -7,19 +7,19 @@ using Object = UnityEngine.Object;
 public class PlayerInteractions : MonoBehaviour
 {
     
-    private static readonly int ApplyInteraction = Animator.StringToHash("ApplyInteraction");
-    private static readonly string CanvasOfInteractable = "CanvaI";
-    private GameObject canvas;
+    // private static readonly int ApplyInteraction = Animator.StringToHash("ApplyInteraction");
+    // private static readonly string CanvasOfInteractable = "CanvaI";
+    // private GameObject canvas;
     
     // Start is called before the first frame update
     void Start()
     {
-        canvas = GameObject.FindWithTag(CanvasOfInteractable); // Find the canvas object
-        print(canvas);
-        if (canvas != null)
-        {
-            canvas.SetActive(false); // Initially deactivate the canvas
-        }
+        // canvas = GameObject.FindWithTag(CanvasOfInteractable); // Find the canvas object
+        // print(canvas);
+        // if (canvas != null)
+        // {
+        //     canvas.SetActive(false); // Initially deactivate the canvas
+        // }
     }
 
     // Update is called once per frame
@@ -28,61 +28,63 @@ public class PlayerInteractions : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-       
-        if (other.tag == "Interaction")
-        {
-            Animator a = other.GetComponentInParent<Animator>();
-            a.SetBool(ApplyInteraction,true);
-            
-        }
-        
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Information"))
-        {
-            if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.I))
-            {
-                if (canvas != null)
-                {
-                    canvas.SetActive(!canvas.activeSelf); // Toggle the canvas
-                }
-            }
-        }
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //    
+    //     if (other.tag == "Interaction")
+    //     {
+    //         Animator a = other.GetComponentInParent<Animator>();
+    //         a.SetBool(ApplyInteraction,true);
+    //         
+    //     }
+    //     
+    // }
+    //
+    
+    // private void OnTriggerStay(Collider other)
+    // {
+    //     if (other.CompareTag("Information"))
+    //     {
+    //         if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.I))
+    //         {
+    //             if (canvas != null)
+    //             {
+    //                 canvas.SetActive(!canvas.activeSelf); // Toggle the canvas
+    //             }
+    //         }
+    //     }
+    //
+    //     if (other.CompareTag("Interaction"))
+    //     {
+    //         Animator animator = other.GetComponentInParent<Animator>();
+    //         if (animator != null)
+    //         {
+    //             animator.SetBool(ApplyInteraction, true);
+    //         }
+    //     }
+    // }
 
-        if (other.CompareTag("Interaction"))
-        {
-            Animator animator = other.GetComponentInParent<Animator>();
-            if (animator != null)
-            {
-                animator.SetBool(ApplyInteraction, true);
-            }
-        }
-    }
 
+    // private void OnTriggerExit(Collider other)
+    // {
+    //     if (other.CompareTag("Interaction"))
+    //     {
+    //         Animator animator = other.GetComponentInParent<Animator>();
+    //         if (animator != null)
+    //         {
+    //             animator.SetBool(ApplyInteraction, false);
+    //         }
+    //     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Interaction"))
-        {
-            Animator animator = other.GetComponentInParent<Animator>();
-            if (animator != null)
-            {
-                animator.SetBool(ApplyInteraction, false);
-            }
-        }
-
-        if (other.CompareTag("Information"))
-        {
-            if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.I))
-            {
-                if (canvas != null)
-                {
-                    canvas.SetActive(false); // Deactivate the canvas
-                }
-            }
-        }
-    }
+        // if (other.CompareTag("Information"))
+        // {
+        //     if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.I))
+        //     {
+        //         if (canvas != null)
+        //         {
+        //             canvas.SetActive(false); // Deactivate the canvas
+        //         }
+        //     }
+        // }
+    // }
 }
