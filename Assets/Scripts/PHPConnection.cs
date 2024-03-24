e1828b428eb821140448286724a37bcf7cf25a93
mahdi <mahdihadjsassi@fsb.u-carthage.tn>
2024-03-24 01:07:33 +0000

    cnx mysql php

---
+++ Assets/Scripts/PHPConnection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PHPConnection : MonoBehaviour
+{
+    private void Start()
+    {
+        // StartCoroutine(GetDate());
+        StartCoroutine(Login("testuser","123456"));
+    }
+
+    IEnumerator GetDate()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:1234/test.php"))
+        {
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+    
+    
+    IEnumerator GetUsers()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:1234/test.php"))
+        {
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+
+    
+    IEnumerator Login(string username, string pass)
+    {
+
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser",username);
+        form.AddField("loginPass",pass);
+        
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:1234/login.php",form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+
+
+    
+}
---
+++ Backend/login.php
@@ -0,0 +1,38 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "Magali_1984";
+$dbname = "unitytuto";
+
+
+//variables submitted by user
+$loginUser= $_POST["loginUser"] ;
+$loginPass= $_POST["loginPass"];
+
+//create connection 
+$conn = new mysqli($servername,$username,$password, $dbname);
+
+//check connection
+if($conn -> connect_error){
+    die("Connection Failed: ".$conn -> connect_error);
+}
+
+$sql = "select password from users where username = '".$loginUser."'";
+
+$result = $conn -> query($sql);
+
+if ($result->num_rows > 0){
+    while($row = $result -> fetch_assoc()){
+        if($row["password"] == $loginPass){
+            echo "Login Success.";
+        }else{
+            echo "Wrong Credentials.";
+        }
+    }
+} else {
+    echo "Username don't exist";
+}
+
+$conn-> close();
+?>
